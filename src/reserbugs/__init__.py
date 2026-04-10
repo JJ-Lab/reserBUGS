91ef9479e6346ba605348cb1375f94c0090aea30
Alfonso Allen-Perkins <alfonso.allen.perkins@gmail.com>
2026-04-10 12:21:08 +0200

    Update __init__.py

--- src/reserbugs/__init__.py
+++ src/reserbugs/__init__.py
@@ -4,7 +4,7 @@ Python Library to execute predictive models of pollinators abundance
 """
 
 from .reservoir_computing import ReservoirComputing
-from .data import CopernicusDataRetriever, ModisDataRetriever
+from .data import CopernicusDataRetriever, ModisDataRetriever, ModisRetrieverConfig
 from .visualization import visualizations
 from .evaluation import type_s_error, type_m_error, scoring_rules
 
@@ -12,6 +12,7 @@ __all__ = [
     "ReservoirComputing",
     "CopernicusDataRetriever",
     "ModisDataRetriever",
+    "ModisRetrieverConfig",
     "visualizations",
     "type_s_error",
     "type_m_error",
